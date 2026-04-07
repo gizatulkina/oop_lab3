d578812d5a6c676e4f7234f7cf42a01ec9fecf8c
gizatulkina <yulia.giza7ullina@yandex.ru>
2026-04-07 12:54:56 +0500

    Полный код программы. Реализовала класс MainWindow с интерфейсом и элементами управления и добавила взаимодействие модели и UI, бизнес-логику и обновление интерфейса

---
+++ lab3_2.py
@@ -0,0 +1,203 @@
+import sys
+import json
+import os
+from PyQt6.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+    QLineEdit, QSlider, QSpinBox, QLabel
+)
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIntValidator
+
+
+# Класс модели, который хранит значения A, B, C и обеспечивает их взаимные ограничения.
+class Model:
+    MIN_VALUE = 0
+    MAX_VALUE = 100
+
+    def __init__(self):
+        self.a = 0
+        self.b = 50
+        self.c = 100
+        self.listeners = []
+
+        self.load()  # загрузка
+
+    def subscribe(self, callback):
+        self.listeners.append(callback)
+        callback(self.a, self.b, self.c)  # одно уведомление при старте
+
+    def notify(self):
+        for cb in self.listeners:
+            cb(self.a, self.b, self.c)
+
+    def _clamp(self, value):
+        return max(self.MIN_VALUE, min(self.MAX_VALUE, value))
+
+    # A 
+    def set_a(self, value):
+        value = self._clamp(value)
+        old = (self.a, self.b, self.c)
+
+        self.a = value
+        if self.a > self.b:
+            self.b = self.a
+        if self.b > self.c:
+            self.c = self.b
+
+        if (self.a, self.b, self.c) != old:
+            self.notify()
+
+    # B
+    def set_b(self, value):
+        value = self._clamp(value)
+        old = (self.a, self.b, self.c)
+
+        if value < self.a:
+            value = self.a
+        if value > self.c:
+            value = self.c
+
+        self.b = value
+
+        if (self.a, self.b, self.c) != old:
+            self.notify()
+
+    # C
+    def set_c(self, value):
+        value = self._clamp(value)
+        old = (self.a, self.b, self.c)
+
+        self.c = value
+        if self.c < self.b:
+            self.b = self.c
+        if self.b < self.a:
+            self.a = self.b
+
+        if (self.a, self.b, self.c) != old:
+            self.notify()
+
+    # сохранение только A и C
+    def save(self):
+        path = os.path.join(os.path.dirname(__file__), "data.json")
+        with open(path, "w") as f:
+            json.dump({"a": self.a, "c": self.c}, f)
+
+    # загрузка (B не сохраняется) 
+    def load(self):
+        path = os.path.join(os.path.dirname(__file__), "data.json")
+        try:
+            with open(path, "r") as f:
+                data = json.load(f)
+                self.a = self._clamp(data.get("a", 0))
+                self.c = self._clamp(data.get("c", 100))
+
+                # B задаём заново и приводим к диапазону A ≤ B ≤ C
+                self.b = max(self.a, min(50, self.c))
+        except:
+            self.a = 0
+            self.c = 100
+            self.b = 50
+
+
+# Класс главного окна
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.model = Model()
+
+        self.setWindowTitle("MVC A B C")
+        self.resize(600, 300)
+
+        self.layout = QVBoxLayout()
+        self.controls = {}
+
+        # создание UI
+        for name in ["A", "B", "C"]:
+            row = QHBoxLayout()
+
+            label = QLabel(name)
+            line = QLineEdit()
+            slider = QSlider(Qt.Orientation.Horizontal)
+            spin = QSpinBox()
+
+            slider.setRange(Model.MIN_VALUE, Model.MAX_VALUE)
+            spin.setRange(Model.MIN_VALUE, Model.MAX_VALUE)
+            line.setValidator(QIntValidator(Model.MIN_VALUE, Model.MAX_VALUE))
+
+            row.addWidget(label)
+            row.addWidget(line)
+            row.addWidget(slider)
+            row.addWidget(spin)
+
+            self.layout.addLayout(row)
+            self.controls[name] = (line, slider, spin)
+
+        self.setLayout(self.layout)
+
+        # события
+        self.controls["A"][0].editingFinished.connect(lambda: self.edit("A"))
+        self.controls["A"][1].valueChanged.connect(lambda v: self.model.set_a(v))
+        self.controls["A"][2].valueChanged.connect(lambda v: self.model.set_a(v))
+
+        self.controls["B"][0].editingFinished.connect(lambda: self.edit("B"))
+        self.controls["B"][1].valueChanged.connect(lambda v: self.model.set_b(v))
+        self.controls["B"][2].valueChanged.connect(lambda v: self.model.set_b(v))
+
+        self.controls["C"][0].editingFinished.connect(lambda: self.edit("C"))
+        self.controls["C"][1].valueChanged.connect(lambda v: self.model.set_c(v))
+        self.controls["C"][2].valueChanged.connect(lambda v: self.model.set_c(v))
+
+        # подписка в конце
+        self.model.subscribe(self.update_ui)
+
+    
+    def get_int(self, line):
+        try:
+            return int(line.text())
+        except:
+            return None
+
+    def edit(self, name):
+        line = self.controls[name][0]
+        value = self.get_int(line)
+        if value is None:
+            return
+
+        if name == "A":
+            self.model.set_a(value)
+        elif name == "B":
+            self.model.set_b(value)
+        else:
+            self.model.set_c(value)
+
+    def update_ui(self, a, b, c):
+        for line, slider, spin in self.controls.values():
+            line.blockSignals(True)
+            slider.blockSignals(True)
+            spin.blockSignals(True)
+
+        self.set_values("A", a)
+        self.set_values("B", b)
+        self.set_values("C", c)
+
+        for line, slider, spin in self.controls.values():
+            line.blockSignals(False)
+            slider.blockSignals(False)
+            spin.blockSignals(False)
+
+    def set_values(self, name, value):
+        line, slider, spin = self.controls[name]
+        line.setText(str(value))
+        slider.setValue(value)
+        spin.setValue(value)
+
+    def closeEvent(self, event):
+        self.model.save()
+        event.accept()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
